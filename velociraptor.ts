e458d102e928fe2129ec19cd9f58c260b9f3f93b
Rodolfo Beccari de Oliveira <rod.beccari@gmail.com>
2021-09-01 14:48:59 -0300

    feat(git-aliases): adds git alias file, and changes 'install_docker_aliases' to 'install_aliases'

--- velociraptor.ts
+++ velociraptor.ts
@@ -15,9 +15,9 @@ export default <ScriptsConfiguration> {
       ...common,
       allow: ["all"],
     },
-    install_docker_aliases: {
-      cmd: "scripts/install_aliases.ts docker",
-      desc: "Adds to your shell config file Docker and Docker Compose aliases",
+    install_aliases: {
+      cmd: "scripts/install_aliases.ts docker git",
+      desc: "Adds all aliases to your shell config file",
       ...common,
     },
     install_self_update: {
